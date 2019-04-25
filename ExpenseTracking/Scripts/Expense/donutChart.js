f41998ec2d3eb1d3f462530a3234182815897cb1
Bryan Teng <bteng1226@gmail.com>
2019-04-25 10:48:20 -0500

    added chart functionality, hard coded for now

---
+++ ExpenseTracking/Scripts/Expense/donutChart.js
@@ -0,0 +1,45 @@
+﻿let pieData = [
+    {
+        value: 121.45,
+        label: "groceries"
+    },
+    {
+        value: 70,
+        label: "shoes"
+    }, {
+        value: 9.55,
+        label: "food"
+    }
+];
+
+function dataToChart(data) {
+    let temp = []
+    for (let i = 0; i < data.length; i++) {
+        temp.push(getRandomColor())
+    }
+    return {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: data.map(x => x.value),
+                backgroundColor: temp,
+            }],
+            labels: data.map(x => x.label)
+        },
+        options: {
+            responsive: true
+        }
+    }
+}
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+window.onload = function () {
+    var ctx = document.getElementById("donutChart").getContext("2d");
+    window.myPie = new Chart(ctx, dataToChart(pieData));
+};
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# ExpenseTracking" 
+"# ExpenseTracking" 
+# ExpenseTracking
